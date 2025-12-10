8db2097a64fe2cd6edda1d3832cec6e18d05224c
AB45699 <b.anam8541@gmail.com>
2025-12-10 23:21:08 +0000

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import './App.css';
-import Header from './components/filter-options/header/Header.jsx';
 import AllPropertiesPage from './components/properties-grid/AllPropertiesPage.jsx';
-import { useSearchParams } from 'react-router-dom';
 
 function App() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  
   return (
-    <>
-    <Header searchParams={searchParams} setSearchParams={setSearchParams}/>
-    <AllPropertiesPage searchParams={searchParams} setSearchParams={setSearchParams}/>
-    </>
-    
+    <AllPropertiesPage/>
   )
 }
 
