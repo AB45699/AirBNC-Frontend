064eaa567107daffdb1a11e85e7c53918df4dcc3
AB45699 <b.anam8541@gmail.com>
2025-12-11 23:23:45 +0000

    refactoring

--- src/components/filter-options/PropertyTypeFilters.jsx
+++ src/components/filter-options/PropertyTypeFilters.jsx
@@ -1,23 +1,22 @@
 import checkmark from '../../assets/check.png';
 
 function PropertyTypeFilters({propertyTypeQuery, setPropertyTypeQuery}) {
-    const propertyTypes = ["Apartment", "House", "Studio", "Loft", "Villa", "Cottage", "Chalet", "Cabin", "Mansion", "Castle", "Bungalow", "Townhouse", "Penthouse", "Duplex"]
-    
+    const propertyTypes = ["Apartment", "House", "Studio", "Loft", "Villa", "Cottage", "Chalet", "Cabin", "Mansion", "Castle", "Bungalow", "Townhouse", "Penthouse", "Duplex"];
     
     return (
         <div className="property-type-filter-container">
             <p>Property types</p>
-            {propertyTypes.map((propertyType)=>{
-                return <button 
-                        type="button" 
-                        key={propertyType}
-                        className={propertyTypeQuery !== propertyType ? "property-type-option-selector" : "property-type-option-selector selected"}
-                        onClick={()=>{setPropertyTypeQuery(propertyType)
-                        }}>
-                    {propertyType} {propertyTypeQuery === propertyType ? <img className="tick" src={checkmark} alt="tick"/> : ""}
-                    </button>
-            })}
-
+                {propertyTypes.map((propertyType) => {
+                    return <button 
+                                type="button" 
+                                key={propertyType}
+                                className={`property-type-option-selector ${propertyTypeQuery === propertyType ? "selected" : ""}`}
+                                onClick={()=>{setPropertyTypeQuery(propertyType)}}
+                            >
+                                {propertyType} 
+                                {propertyTypeQuery === propertyType && (<img className="tick" src={checkmark} alt="tick icon"/> )}
+                            </button>
+                })}
         </div>
     )
 }; 
