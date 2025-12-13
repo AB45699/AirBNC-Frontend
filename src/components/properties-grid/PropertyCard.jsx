f798d47a4426ba3cd8be6c1e339062ed748fe8c1
AB45699 <b.anam8541@gmail.com>
2025-12-13 22:15:35 +0000

    linking to single property page

--- src/components/properties-grid/PropertyCard.jsx
+++ src/components/properties-grid/PropertyCard.jsx
@@ -1,26 +1,29 @@
 import "./PropertiesGrid.css";
 import FavouritesIcon from "./FavouritesIcon.jsx";
 import starIcon from '../../assets/star-icon.png';
+import { Link } from "react-router-dom"; 
 
 function PropertyCard( {property} ) {
     return (
-        <div className="property-card">
+        <Link to="/properties/:property_id">
+            <div className="property-card">
 
-            <div className="property-card-image-wrapper">
-                <FavouritesIcon property={property}/>
-                <img className="property-card-image" src={property.image} alt="image of property"/>
+                <div className="property-card-image-wrapper">
+                    <FavouritesIcon property={property}/>
+                    <img className="property-card-image" src={property.image} alt="image of property"/>
+                </div>
+
+                <div className="property-card-info"> 
+                    <p style={{fontWeight:"bold"}}>{property.property_name} </p>
+                    <p>{property.location}</p>
+                    <p> £{property.price_per_night} per night •  
+                        <span style={{fontWeight:"normal"}}> {property.avg_rating?.slice(0, 4) || "0"} </span>
+                        <span><img className="star-icon" src={starIcon} alt="star-icon"/></span>
+                    </p>
+                </div>
+
             </div>
-
-            <div className="property-card-info"> 
-                <p style={{fontWeight:"bold"}}>{property.property_name} </p>
-                <p>{property.location}</p>
-                <p> £{property.price_per_night} per night •  
-                    <span style={{fontWeight:"normal"}}> {property.avg_rating?.slice(0, 4) || "0"} </span>
-                    <span><img className="star-icon" src={starIcon} alt="star-icon"/></span>
-                </p>
-            </div>
-
-        </div>
+        </Link>
     )
 };
 
