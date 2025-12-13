50fd4a69052fc4d4d692f82a3c9c70800c730c1c
AB45699 <b.anam8541@gmail.com>
2025-12-13 20:31:24 +0000

    adding avg rating

--- src/components/properties-grid/PropertyCard.jsx
+++ src/components/properties-grid/PropertyCard.jsx
@@ -1,5 +1,6 @@
 import "./PropertiesGrid.css";
 import FavouritesIcon from "./FavouritesIcon.jsx";
+import starIcon from '../../assets/star-icon.png';
 
 function PropertyCard( {property} ) {
     return (
@@ -11,12 +12,11 @@ function PropertyCard( {property} ) {
             <div className="property-card-info"> 
                 <p style={{fontWeight:"bold"}}>{property.property_name} </p>
                 <p>{property.location}</p>
-                <p> £{property.price_per_night} • {property.host} </p>
-                
+                <p> £{property.price_per_night} per night •  
+                    <span style={{fontWeight:"normal"}}> {property.avg_rating?.slice(0, 4) || "0"} </span>
+                    <span><img  className="star-icon" src={starIcon} alt="star-icon"/></span></p>
             </div>
         </div>
-
-        
     )
 };
 
