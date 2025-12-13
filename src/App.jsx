fbd52562071884fdb98f0534dbe1a32557c4aea5
AB45699 <b.anam8541@gmail.com>
2025-12-13 22:44:32 +0000

    setting up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import AllPropertiesPage from './components/properties-grid/AllPropertiesPage.jsx';
+import { Routes, Route } from 'react-router-dom'; 
+import SinglePropertyPage from './components/single-property/SinglePropertyPage.jsx';
 
 function App() {
   return (
-    <AllPropertiesPage/>
+    <>
+    <Routes>
+      <Route path="/" element={<AllPropertiesPage/>}/>
+      <Route path="/properties/:property_id" element={ <SinglePropertyPage />}/>
+    </Routes>
+    </>
   )
 }
 
