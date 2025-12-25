3c99c9c874df9b690e6405264fae2417a24ed780
AB45699 <b.anam8541@gmail.com>
2025-12-25 19:13:33 +0000

    changed favourites key

--- src/components/properties-grid/FavouritesIcon.jsx
+++ src/components/properties-grid/FavouritesIcon.jsx
@@ -4,7 +4,7 @@ import './PropertiesGrid.css';
 function FavouritesIcon( {property} ) {
     return (
         <button type="button" className="favourite-button">
-            <p className="num-of-faves"> {property.num_of_faves}</p>
+            <p className="num-of-faves"> {property.favourites}</p>
             <img className="empty-heart" src={emptyHeart} alt="empty heart"/>
         </button>
     )
