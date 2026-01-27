6a6b664b961a2714abace9cf3e10639b81f89582
AB45699 <b.anam8541@gmail.com>
2026-01-27 20:52:49 +0000

    setting up images carousel

--- src/components/single-property/ImagesCarousel.jsx
+++ src/components/single-property/ImagesCarousel.jsx
@@ -1,8 +1,43 @@
-function ImagesCarousel() {
+import useEmblaCarousel from 'embla-carousel-react';
+import {useState} from 'react'; 
+import './SingleProperty.css';
+
+function ImagesCarousel( {property} ) {
+    const [emblaRef] = useEmblaCarousel();
+    const propertyImages = property.images;
+    const [counter, setCounter] = useState(0);
+
+    function incrementCounter() {
+        setCounter((currentCounter) => {
+            if (currentCounter === propertyImages.length - 1) {
+                return 0
+            } else {
+                return currentCounter +=1
+            }
+        });
+    }; 
+
+    function decrementCounter() {
+        setCounter((currentCounter) => {
+            if (currentCounter === 0) {
+                return (propertyImages.length - 1)
+            } else {
+                return currentCounter -=1
+            }
+        });
+    }
+
     return (
-        <div className="single-property-images">
-            Images carousel placeholder
+        <div className="embla">
+            <div className="embla__viewport" ref={emblaRef}>
+                <div className="embla__container">
+                    <div className="embla__slide"><img className="single-property-image" src={propertyImages[counter]}/></div>
+                </div>
             </div>
+
+            <button className="embla__prev" onClick={decrementCounter}>Scroll to prev</button>
+            <button className="embla__next" onClick={incrementCounter}>Scroll to next</button>
+        </div>
     )
 };
 
--- src/components/single-property/SinglePropertyPage.jsx
+++ src/components/single-property/SinglePropertyPage.jsx
@@ -2,8 +2,6 @@ import { useParams } from 'react-router-dom';
 import { getSingleProperty } from '../../../api.js';
 import {useEffect, useState} from 'react';
 import './SingleProperty.css';
-import CloseButton from '../CloseButton.jsx';
-import {Link} from 'react-router-dom';
 import PropertyDescription from './PropertyDescription.jsx';
 import ImagesCarousel from './ImagesCarousel.jsx';
 
@@ -35,7 +33,7 @@ function SinglePropertyPage() {
 
     return (
         <>
-            <ImagesCarousel />
+            <ImagesCarousel property={property}/>
             <PropertyDescription property={property}/>
         </>
     )
