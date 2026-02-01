78fb487ce8ffaaa50bc88463677d950c97afa509
AB45699 <b.anam8541@gmail.com>
2026-02-01 16:51:28 +0000

    no reviews handling

--- src/components/single-property/ReviewSummary.jsx
+++ src/components/single-property/ReviewSummary.jsx
@@ -3,11 +3,11 @@ import GoldStar from '../../assets/gold-star-icon.png';
 function ReviewSummary( {property} ) {
     return (
         <div className="single-property-details__review"> 
-        { property.review_count !== "0" ? (
+        { property.review_count !== null ? (
             <>
                 <div className="single-property-details__review-average">
                     {property.avg_rating}
-                    {<img className="single-property-details__gold-star" src={GoldStar} alt="gold star image"/>}
+                    <img className="single-property-details__gold-star" src={GoldStar} alt="gold star image"/>
                 </div>
                 <div className="single-property-details__review-count">
                     {property.review_count}
