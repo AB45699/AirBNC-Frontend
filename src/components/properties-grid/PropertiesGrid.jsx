6dda0af7a23a4e5630857e2537ef5f2219191042
AB45699 <b.anam8541@gmail.com>
2026-02-10 23:56:51 +0000

    reverting code

--- src/components/properties-grid/PropertiesGrid.jsx
+++ src/components/properties-grid/PropertiesGrid.jsx
@@ -1,10 +1,36 @@
-import useAllPropertiesData from "../../hooks/useAllPropertiesData.js";
+import {getProperties} from "../../../api.js";
 import PropertyCard from "./PropertyCard.jsx";
 import './PropertiesGrid.css';
+import {useEffect, useState} from "react";
 
 function PropertiesGrid({searchParams}) {
-    const {allProperties, isLoading, hasErrored} = useAllPropertiesData(searchParams);
+    const [allProperties, setAllProperties] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasErrored, setHasErrored] = useState(null);
+
+    const orderQuery = searchParams.get("order");
+    const sortByQuery = searchParams.get("sort");
+    const propertyTypeQuery = searchParams.get("property_type");
+    const minPriceQuery = searchParams.get("minprice");
+    const maxPriceQuery = searchParams.get("maxprice");
+
+    const fetchProperties = async () => {
+        try {
+            const fetchedProperties = await getProperties(orderQuery, sortByQuery, propertyTypeQuery, minPriceQuery, maxPriceQuery);
+            setAllProperties(fetchedProperties); 
+            setIsLoading(false);
+        } catch (err) {
+            setHasErrored(err);
+            setIsLoading(false);
+        }
+        
+    };
+
+    useEffect(()=>{
+        fetchProperties();
+    }, [orderQuery, sortByQuery, propertyTypeQuery, minPriceQuery, maxPriceQuery])
    
+
     if (isLoading) {
         return <p>Loading...</p>
     };
